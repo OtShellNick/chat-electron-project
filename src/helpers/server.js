73e0b8ecc1edd2f4be738985bde05a9707cea47f
Aleksandr <exte92@gmail.com>
2022-06-20 20:08:14 +0300

    up

--- src/helpers/server.js
+++ src/helpers/server.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import * as CookieHelper from '@helpers/cookie'
 import { CONFIG } from '@config/config'
-const server = CONFIG.development.REST
+const server = CONFIG.production.REST
 const Authorization = CookieHelper.get('authorization') || ''
 axios.defaults.headers.common.authorization = Authorization
 
