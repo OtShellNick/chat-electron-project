32221c2adec0895fad2eeab7635c18b45ce980c5
Aleksandr <exte92@gmail.com>
2022-06-20 20:05:35 +0300

    up

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
   production: {
-    REST: 'http://localhost:4112/',
-    SOCKET: 'ws://localhost:4112/'
+    REST: 'https://trovo-bot-server.herokuapp.com/',
+    SOCKET: 'wss://trovo-bot-server.herokuapp.com/'
   },
   development: {
     REST: 'http://localhost:4112/',
