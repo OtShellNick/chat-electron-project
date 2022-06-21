54ef6fa2df6f0b69ee649d7d8e0f2faea6342264
Aleksandr <exte92@gmail.com>
2022-06-21 13:17:28 +0300

    up auth

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,9 @@
-import React, {lazy, useEffect} from 'react'
+import React, {lazy, useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 import {Routes, Route, useLocation, useNavigate} from 'react-router-dom';
 import * as CookieHelper from '@helpers/cookie';
+import {getSelf, login} from "@requests/user";
+import {loginUser} from "@store/userStore/userStore";
 
 import Layout from '@components/Layout/Layout';
 
@@ -10,13 +13,32 @@ const Dashboard = lazy(() => import('@components/Dashboard/Dashboard'));
 import '@styles/main.scss';
 
 const App = () => {
-    const auth = CookieHelper.get('authorization');
-    const location = useLocation();
+    const dispatch = useDispatch();
     const navigate = useNavigate();
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search).get('code');
+    const auth = CookieHelper.get('authorization');
+    const {user} = useSelector((state) => state.user);
 
     useEffect(() => {
-        if(!auth && location.pathname !== '/login') navigate('/login');
-    }, [auth])
+        if (searchParams) {
+            login(searchParams).then((user) => {
+                dispatch(loginUser(user));
+                CookieHelper.set('authorization', user.jwt);
+                navigate('/');
+            });
+        }
+
+        if(!searchParams && !auth && location.pathname !== '/login') navigate('/login');
+
+        if (!user.id) {
+            getSelf().then((user) => {
+                dispatch(loginUser(user));
+            });
+        }
+
+        if(user.id && location.pathname === '/login') navigate('/');
+    }, [searchParams, user, auth]);
 
     return <Routes>
         <Route path='/' element={<Layout/>}>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,54 +1,15 @@
-import React, {Suspense, useEffect} from "react";
-import {Outlet, useLocation, useNavigate} from 'react-router-dom';
-import {useDispatch, useSelector} from 'react-redux';
-import * as CookieHelper from '@helpers/cookie';
+import React, {Suspense} from "react";
+import {Outlet} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 import Preloader from "@components/Preloader/Preloader";
 import Header from "@components/Layout/Header/Header";
 import Aside from "@components/Layout/Aside/Aside";
 
-import {loginUser} from '@store/userStore/userStore';
-import {getSelf, login, refreshToken} from "@/requests/user";
-
 import './Layout.scss';
 
 const Layout = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search).get('code');
-    const auth = CookieHelper.get('authorization');
     const {user} = useSelector((state) => state.user);
-
-    useEffect(() => {
-        if (searchParams) {
-            login(searchParams).then((user) => {
-                dispatch(loginUser(user));
-                CookieHelper.set('authorization', user.jwt);
-                navigate('/');
-            });
-        }
-
-        if(!searchParams && !auth && location.pathname !== '/login') navigate('/login');
-
-        if (!user.id) {
-            getSelf().then((user) => {
-                dispatch(loginUser(user));
-            });
-        }
-
-        if(user.id && location.pathname === '/login') navigate('/');
-    }, [searchParams, user]);
-
-    useEffect(() => {
-        if(auth) {
-            setInterval(() => {
-                refreshToken(auth).then((user) => {
-                    dispatch(loginUser(user));
-                })
-            }, 1000 * 60 * 60 * 2);
-        }
-    }, [auth])
 
     return <div className='layout'>
         <Aside user={user}/>
